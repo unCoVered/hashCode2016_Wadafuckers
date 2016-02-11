a105a60285dd36088b055c87b84c602dbe8f6eac
Alejandro Gálvez <alexgalvez8292@outlook.com>
2016-02-11 21:23:45 +0100

    Beans v3

---
+++ hashCode/src/bean/OrderBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+import java.util.List;
+
+import Helper.Location;
+
+public class OrderBean extends BaseBean
+{
+	private List<ProductBean> itemsList;
+	private Location deliveryLocation;
+	private List<DronBean> deliveryDrones;
+	
+	public List<ProductBean> getItemsList ()
+	{
+		return itemsList;
+	}
+	public void setItemsList (List<ProductBean> itemsList)
+	{
+		this.itemsList = itemsList;
+	}
+	public Location getDeliveryLocation ()
+	{
+		return deliveryLocation;
+	}
+	public void setDeliveryLocation (Location deliveryLocation)
+	{
+		this.deliveryLocation = deliveryLocation;
+	}
+	public List<DronBean> getDeliveryDrones ()
+	{
+		return deliveryDrones;
+	}
+	public void setDeliveryDrones (List<DronBean> deliveryDrones)
+	{
+		this.deliveryDrones = deliveryDrones;
+	}
+}
