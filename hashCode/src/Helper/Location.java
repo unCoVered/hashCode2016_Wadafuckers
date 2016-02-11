0bc916b16c088a1a682a4af81e9cbc52f12f38cd
Alejandro Gálvez <alexgalvez8292@outlook.com>
2016-02-11 21:13:13 +0100

    changes in location

--- hashCode/src/Helper/Location.java
+++ hashCode/src/Helper/Location.java
@@ -5,6 +5,12 @@ public class Location
 	private String column;
 	private String row;
 	
+	public Location (String column, String row)
+	{
+		this.column = column;
+		this.row = row;
+	}
+	
 	public String getColumn ()
 	{
 		return column;
