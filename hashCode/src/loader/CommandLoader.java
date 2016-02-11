06eeabba5f69d9ab88ccf124629ed8cf34bdf61b
Ramon Jose Arellano Bitian <594040@unizar.es>
2016-02-11 22:28:25 +0100

    just do ti

--- hashCode/src/loader/CommandLoader.java
+++ hashCode/src/loader/CommandLoader.java
@@ -1,6 +1,28 @@
 package loader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CommandLoader
 {
+
+    private static Scanner sc;
+    public static void LoadCommand (File commandInput)
+    {
+        try
+        {
+            sc = new Scanner(commandInput);
+            int numberOfCommands;
+            while(sc.hasNextLine())
+            {
+                numberOfCommands = sc.nextInt();
+                sc.nextLine();  //End Line
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
